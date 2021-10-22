299109b0aadaaae6bfaf2754177a8df2ce778f76
alvissraghnall <isiomastewart@gmail.com>
2021-10-22 14:57:38 +0100

    next

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-module.exports  = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/vue/dist/' : '/'
-}
-
-
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/vue/dist/" : "/",
+};
