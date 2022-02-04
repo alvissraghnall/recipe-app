5a9629e70cc9c3804e197ba4e24dd5970260ad41
alvissraghnall <isiomastewart@gmail.com>
2022-02-04 01:41:33 +0100

    Dua Lipa - Levitating

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="home">
-    <p>Jesus Walks!</p>
+  <div class="home" :class="isMobile ? 'home-sm' : 'home-lg'">
+    <div class="home-sm-intro">
+      <h3 :class="$tt('headline3')" class="headline-main" style="top: 70%;"> Cooking Experience Like A Chef </h3>
+      <h6 :class="$tt('overline')"> Let's make a delicious dish with the best recipe for the family. </h6>
+
+      <ui-button icon="arrow_circle_right" :class="$tt('button')" style="background: green; color: white; height: 40px; border-radius: 32px; padding: 26px;"> Get Started </ui-button>
+    </div>
   </div>
 </template>
 
@@ -13,13 +18,17 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 export default class Home extends Vue {
   message = "Hello"
   public windowWidth: number = window.innerWidth;
+  public isMobile = true;
 
   public mounted(){
     window.addEventListener("resize", this.handleResize);
   }
 
   public handleResize(){
+    console.log(this.windowWidth, "\n=====\n");
     this.windowWidth = window.innerWidth;
+    this.isMobile = this.windowWidth < 800;
+    console.log(this.windowWidth);
   }
 
   public beforeDestroy(){
@@ -29,12 +38,53 @@ export default class Home extends Vue {
 </script>
 
 <style lang="scss">
+//
+// @use '@material/button/button-shared-theme' with (
+//   $height: 45px,
+//   $horizontal-padding: 8px,
+//   $minimum-height: 33px,
+//   $maximum-height: 50px
+//
+// );
 
 .home {
-  background-image: url("../assets/bg-lg.jpg");
   height: 100vh;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
+  position: relative;
+
+  &-lg {
+    background-image: url("../assets/bg-lg.jpg");
+  }
+
+  &-sm {
+    background-image: url("../assets/bg-sm.jpg");
+
+    &-intro {
+      position: absolute;
+      text-align: center;
+      color: cyan;
+      top: 70%;
+      left: 40%;
+      transform: translate(-40%, -70%);
+    }
+  }
+}
+
+.headline-main {
+  font-family: sans-serif !important;
+  font-size: 2.6em !important;
+  font-weight: 900 !important;
+}
+
+@media screen and (min-width: 801px) {
+  .home {
+    background-image: url("../assets/bg-lg.jpg");
+
+    &-sm-intro {
+      display: none;
+    }
+  }
 }
 </style>
