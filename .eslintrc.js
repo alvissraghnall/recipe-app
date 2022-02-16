9f7378d2148dc019f70c283f205abcf99464be3a
Alviss Raghnall <isiomastewart@gmail.com>
2022-02-15 16:54:41 -0800

    NF - Time

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
+  // "parser": "@babel/eslint-parser"
+
 };
--- src/views/GetStarted.vue
+++ src/views/GetStarted.vue
@@ -2,7 +2,11 @@
   <div class="get-started">
     <ui-card outlined class="form-card">
       <div class="sign-up-in">
-        <Tabs :items="this.items" :activeItem="this.activeItem" @tabClick="(event) => this.handleTab(event)" />
+        <Tabs
+          :items="this.items"
+          :activeItem="this.activeItem"
+          @tabClick="(event) => this.handleTab(event)"
+        />
         <div v-if="this.activeItem === 'Sign Up'">
           <SignUpForm />
         </div>
@@ -12,32 +16,34 @@
       </div>
     </ui-card>
   </div>
-
 </template>
 
 <script lang="ts">
-  import { Options, Vue } from "vue-class-component";
-  import Tabs from "../components/Tabs.vue";
-  import SignUpForm from "../components/SignUpForm.vue";
-  import SignInForm from "../components/SignInForm.vue";
-  @Options({
-    components: {
-      Tabs,
-      SignUpForm,
-      SignInForm
-    }
-  })
-  export default class GetStarted extends Vue {
-    protected items: string[] = ["Sign Up", "Sign In"];
-    private activeItem: string = this.items[0];
+import { Options, Vue } from "vue-class-component";
+import Tabs from "../components/Tabs.vue";
+import SignUpForm from "../components/SignUpForm.vue";
+import SignInForm from "../components/SignInForm.vue";
+@Options({
+  components: {
+    Tabs,
+    SignUpForm,
+    SignInForm,
+  },
+})
+export default class GetStarted extends Vue {
+  protected items: string[] = ["Sign Up", "Sign In"];
+  private activeItem: string = this.items[0];
 
-    private handleTab(e: Event): void {
-      console.log(e);
-      
-      this.activeItem = (<HTMLUListElement>e.target).textContent!;
-      console.log(this.activeItem);
-    }
+  private handleTab(e: Event): void {
+    // console.log(e);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.activeItem = (e.target as HTMLUListElement).textContent!;
+    //console.log(this.activeItem);
   }
+  // eslint-disable-next-line prettier/prettier
+}
+// eslint-disable-next-line prettier/prettier
 </script>
 
 <style lang="scss">
@@ -47,13 +53,10 @@ body {
 
 .sign-in-form {
   margin: 15px;
-
 }
 
 .form-card {
   width: 450px;
   margin: 45px;
 }
-
-
 </style>
